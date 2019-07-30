037f568c1e8700389523e3778ab242405cd6dac4
Narathip.Th <tapong.narathip@gmail.com>
2019-07-30 14:52:55 +0700

    connect vue to backend

--- client/src/components/Users/Index.vue
+++ client/src/components/Users/Index.vue
@@ -1,11 +1,33 @@
 <template>
     <div>
-        <h1>Get All Users</h1>
+        <h2>Get All Users</h2>
+        <h4>จำนวนผู้ใช้งาน {{users.length}}</h4>
+        <div v-for="user in users" v-bind:key="user.id">
+            <p>id : {{user.id}}</p>
+            <p>ชื่อ-นามสกุล : {{user.name}} - {{user.lastname}}</p>
+            <p>email : {{user.email}}</p>
+            <p>password : {{user.password}}</p>
+            <hr>
+        </div>
     </div>
 </template>
 <script>
+import UserService from '@/services/UserService'
 export default {
-    
+    // created() {
+    //     this.$http.get('http://localhost:8081/user')
+    //     .then(function (response){
+    //         console.log(response)
+    //     })
+    // }
+    data () {
+        return {
+            users: []
+        }
+    },
+    async created() {
+        this.users = (await UserService.index()).data
+    }
 }
 </script>
 <style scoped>
